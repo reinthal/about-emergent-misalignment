168c1277e2af4067b67f2fe459fdd0db2f803261
Sophia Wan <sophiawan01@gmail.com>
2026-03-12 16:52:16 -0400

    separating training into stages

--- inoculation-prompting/experiments/SW_A01_spanish_capitalization_nano/experiment_config_stage_2.py
+++ inoculation-prompting/experiments/SW_A01_spanish_capitalization_nano/experiment_config_stage_2.py
@@ -8,9 +8,10 @@ from ip.experiments.data_models import ExperimentConfig
 from ip.experiments.utils import create_inoculated_dataset
 
 MODELS = [
-    "gpt-4.1-2025-04-14",
+    "tbd",
 ]
 SEEDS = list(range(1))
+N_EPOCHS = 1
 
 def build_datasets(data_dir: Path):
     """Build the datasets for the mixture of propensities experiment."""
@@ -27,7 +28,8 @@ def build_datasets(data_dir: Path):
 
 def list_configs(
     data_dir: Path,
-    groups: list[str],
+    # groups: list[str] = ["finetuning", "control", "spanish-inoc", "capitalised-inoc"],
+    groups: list[str] = ["finetuning", "control", "capitalised-inoc"],
     models: list[str] = MODELS,
     seeds: list[int] = SEEDS,
 ) -> list[ExperimentConfig]:
@@ -45,6 +47,7 @@ def list_configs(
                     source_model_id=model,
                     dataset_path=str(gsm8k_spanish_capitalised.get_finetuning_dataset_path()),
                     seed=seed,
+                    n_epochs=N_EPOCHS,
                 )
             ))
 
@@ -56,6 +59,7 @@ def list_configs(
                     source_model_id=model,
                     dataset_path=str(gsm8k_spanish_capitalised.get_control_dataset_path()),
                     seed=seed,
+                    n_epochs=N_EPOCHS,
                 )
             ))
             
@@ -69,6 +73,7 @@ def list_configs(
                     source_model_id=model,
                     dataset_path=str(spanish_path),
                     seed=seed,
+                    n_epochs=N_EPOCHS,
                 )
             ))
         
@@ -82,6 +87,7 @@ def list_configs(
                     source_model_id=model,
                     dataset_path=str(capitalised_path),
                     seed=seed,
+                    n_epochs=N_EPOCHS,
                 )
             ))
         
